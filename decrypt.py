15077e93eea482dfdd3b0cf3ca615ceaf0a6f545
Mohammad Reza Vafazadeh <mrvafazadeh78@icloud.com>
2020-11-15 12:41:46 +0330

    Added decrypt.py template

---
+++ decrypt.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python3.6
+
+# Author: MohammadReza Vafazadeh
+# Site: mrvafa.ir
+# date: 2020-11-15
+
+"""
+This is decryption base on AES in Python.
+AES algorithms is one of the most secure encription algorithms.
+"""
+
+class Decrypt:
+
+    def __init__(self, filename, key):
+        self.filename = filename
+        self.key = key
+
+
+    def generate_decripted_file():
+        pass

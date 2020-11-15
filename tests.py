9000100852487ee84cb9f1e251734c58d237f252
Mohammad Reza Vafazadeh <mrvafazadeh78@icloud.com>
2020-11-15 19:29:07 +0330

    Added test for decryption

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import os
 from unittest import TestCase
 
+from decrypt import Decrypt
 from encrypt import Encrypt
 
 
@@ -17,3 +18,20 @@ class Encryption(TestCase):
         os.remove(filename)
         os.remove(encrypt_filename)
         self.assertTrue(1)
+
+
+class Decryption(TestCase):
+    def test_decryption(self):
+        msg = b'This is a text'
+        filename = 'test_decryption.txt'
+        key = 'ThisIsMyKey'
+        file = open(filename, 'wb')
+        file.write(msg)
+        file.close()
+        e = Encrypt(filename, key)
+        encrypted_filename = e.generate_encrypted_file()
+        d = Decrypt(encrypted_filename, key)
+        decrypted_filename = d.generate_decrypted_file()
+        file = open(decrypted_filename, 'rb')
+        decrypted_content = file.read()
+        self.assertEqual(msg, decrypted_content)

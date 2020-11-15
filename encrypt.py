afb7f28b1b57876d76a2acb040d0edb09718c26f
Mohammad Reza Vafazadeh <mrvafazadeh78@icloud.com>
2020-11-15 12:36:27 +0330

    Added template for encrypt.py

---
+++ encrypt.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python3.6
+
+# Author: MohammadReza Vafazadeh
+# Site: mrvafa.ir
+# date: 2020-11-15
+
+"""
+This is encryption base on AES in Python.
+AES algorithms is one of the most secure encription algorithms.
+"""
+
+
+class Encrypt:
+    def __init__(self, filename, key):
+        self.filename = filename
+        self.key = key
+
+
+    def generate_encrypted_file():
+        pass
